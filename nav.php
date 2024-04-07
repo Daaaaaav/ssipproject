8cbebe77fbd4e6eebde7a2178093b738d657532a
Daaaaaav <144467455+Daaaaaav@users.noreply.github.com>
2024-04-07 10:36:19 +0700

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,12 @@
+<?php
+require_once("header.php");
+?>
+
+<nav class="nav">
+   <a class="nav-link" href="index.php">Ferries</a>
+   <a class="nav-link" href="listOwnership.php">Company</a>
+   <a class="nav-link" href="listCountry.php">Country</a>
+   <a class="nav-link" href="listRoom.php">Room</a>
+   <a class="nav-link" href="listPassenger.php">Passenger</a>
+   <a class="nav-link" href="listCrew.php">Crew</a>
+</nav>
